2210247d2b8b60b33f55693ee64236d4fa545ddf
Nandesh Guru <guru@Nandeshs-MacBook-Pro.local>
2016-10-26 21:22:05 -0700

    adding masking support
    for configure command

--- cmd/util/config.go
+++ cmd/util/config.go
@@ -1,14 +1,20 @@
 package util
 
 import (
+	"fmt"
+
 	"github.com/spf13/viper"
 	"github.com/CloudCoreo/cli/cmd/content"
 )
 
 // GetValueFromConfig to get values from config
-func GetValueFromConfig(key string) (value string) {
+func GetValueFromConfig(key string, masked bool) (value string) {
 	if value = viper.GetString(key); value == "" {
 		value = content.NONE
+	} else {
+		if masked {
+			value = maskConfigKey(value)
+		}
 	}
 
 	return value
@@ -19,4 +25,12 @@ func UpdateConfig(key, value string ) {
 	if value != "" {
 		viper.Set(key, value)
 	}
+}
+
+func maskConfigKey(key string) string {
+	if len(key) >= content.MASK_LENGTH {
+		return fmt.Sprintf("%s%s", content.MASK, key[len(key) - content.MASK_LENGTH:])
+	}
+
+	return fmt.Sprintf("%s%s", content.MASK, key)
 }
