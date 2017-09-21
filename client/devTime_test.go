1812efa773489bbb4a3792c0a840f946b07db9a6
Nandesh <nandesh@cloudcoreo.com>
2017-09-20 22:02:48 -0700

    Fixing client test

--- client/devTime_test.go
+++ client/devTime_test.go
@@ -43,7 +43,42 @@ const teamDevTimeSONPayload = `[{
 	}]`
 
 const devTimeJSONPayload = `{
-	"id": "compositeID"
+	"context": "context",
+	"task": "task",
+	"devTimeUrl": "https://2b80f18f-504c-4065-ac24-d722eddc8a0d.devtime.guru.com:9876",
+	"links": [
+		{
+			"ref": "self",
+			"method": "GET",
+			"href": "http://localhost:3000/api/devtime/59c2c54dfd9b7a5d0c6fa4cc"
+		},
+		{
+			"ref": "team",
+			"method": "GET",
+			"href": "http://localhost:3000/api/teams/5994c647af67725e01e4cb8f"
+		},
+		{
+			"ref": "start",
+			"method": "GET",
+			"href": "http://localhost:3000/api/devtime/59c2c54dfd9b7a5d0c6fa4cc/start"
+		},
+		{
+			"ref": "stop",
+			"method": "GET",
+			"href": "http://localhost:3000/api/devtime/59c2c54dfd9b7a5d0c6fa4cc/stop"
+		},
+		{
+			"ref": "results",
+			"method": "GET",
+			"href": "http://localhost:3000/api/devtime/59c2c54dfd9b7a5d0c6fa4cc/results"
+		},
+		{
+			"ref": "jobs",
+			"method": "GET",
+			"href": "http://localhost:3000/api/devtime/59c2c54dfd9b7a5d0c6fa4cc/jobs"
+		}
+	],
+	"devTimeId": "2b80f18f-504c-4065-ac24-d722eddc8a0d"
 }`
 
 func TestCreateProxyTaskSuccess(t *testing.T) {
