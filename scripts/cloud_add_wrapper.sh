c85704283fe5701bfe4dbb152670c181ce99c0c8
zechenjiang <>
2018-12-18 22:56:53 -0800

    PLA-7397: Skip account creation if already exist

--- scripts/cloud_add_wrapper.sh
+++ scripts/cloud_add_wrapper.sh
@@ -6,6 +6,20 @@ team_id="YOUR_TEAM_ID"
 role_name="securestate_role"
 while read account_name account_id environment profile; do
 	echo "creating role for account: $account_id"
+	accounts=$(coreo cloud list --json --team-id $team_id | jq -r .[].name)
+	is_exist=false
+	while read account
+	do
+		if [ "$account" = "$account_name" ]; then
+			is_exist=true
+			break
+		fi
+	done <<< "$accounts"
+	if [ $is_exist = true ]; then
+		echo "Cloud account with name $account_name already exist, skip for this account"
+		continue
+	fi
 	cloud_id=$(coreo cloud add --team-id $team_id --name $account_name --role $role_name --aws-profile $profile --environment $environment --json | jq -r .id)
 	coreo event setup --team-id $team_id --cloud-id $cloud_id --aws-profile $profile
 done
+unset IFS
