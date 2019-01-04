4ddd01e2062bf0dbf93e0e063a6ea0e945556bac
zechenjiang <>
2019-01-04 12:41:27 -0800

    PLA-7459: Add more test code to increase test coverage

--- client/event_test.go
+++ client/event_test.go
@@ -94,3 +94,28 @@ func TestGetRemoveConfigSuccess(t *testing.T) {
 	assert.Nil(t, err, "getRemoveConfig shouldn't return error")
 	assert.Equal(t, "fakeStackName", config.StackName)
 }
+
+func TestGetRemoveConfigFailureWithNoResponse(t *testing.T) {
+	ts := httpstub.New()
+	ts.Path("/api/teams/teamID/cloudaccounts").WithMethod("GET").WithBody(fmt.Sprintf(CloudAccountJSONPayload, ts.URL, ts.URL, ts.URL)).WithStatus(http.StatusOK)
+	ts.Path("/api/users/userID/teams").WithMethod("GET").WithBody(fmt.Sprintf(teamCloudAccountJSONPayload, ts.URL, ts.URL)).WithStatus(http.StatusOK)
+	ts.Path("/me").WithMethod("GET").WithBody(fmt.Sprintf(userJSONPayloadForTeam, ts.URL)).WithStatus(http.StatusOK)
+	ts.Path("/api/cloudaccounts/cloudAccountID/event/remove").WithMethod("GET").WithBody("").WithStatus(http.StatusBadRequest)
+	defer ts.Close()
+
+	client, _ := MakeClient("ApiKey", "SecretKey", ts.URL)
+	_, err := client.GetRemoveConfig(context.Background(), "teamID", "cloudAccountID")
+	assert.NotNil(t, err, "getRemovepConfig should return error")
+}
+
+func TestGetRemoveConfigFailureWithNoLink(t *testing.T) {
+	ts := httpstub.New()
+	ts.Path("/api/teams/teamID/cloudaccounts").WithMethod("GET").WithBody(fmt.Sprintf(CloudAccountJSONPayloadNoSetup, ts.URL)).WithStatus(http.StatusOK)
+	ts.Path("/api/users/userID/teams").WithMethod("GET").WithBody(fmt.Sprintf(teamCloudAccountJSONPayload, ts.URL, ts.URL)).WithStatus(http.StatusOK)
+	ts.Path("/me").WithMethod("GET").WithBody(fmt.Sprintf(userJSONPayloadForTeam, ts.URL)).WithStatus(http.StatusOK)
+
+	client, _ := MakeClient("ApiKey", "SecretKey", ts.URL)
+	_, err := client.GetRemoveConfig(context.Background(), "teamID", "cloudAccountID")
+	assert.NotNil(t, err, "getRemoveConfig should return error")
+	assert.Equal(t, "resource for given ID not found", err.Error())
+}
