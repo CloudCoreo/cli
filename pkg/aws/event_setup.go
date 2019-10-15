0256303419d35d1cea93e71ee08e1512f0f4c5b5
Zechen Jiang <jiangz@vmware.com>
2019-10-15 15:26:59 -0700

    Add comment to avoid future error

--- pkg/aws/event_setup.go
+++ pkg/aws/event_setup.go
@@ -129,6 +129,7 @@ func (a *SetupService) newTag(key, value string) *cloudformation.Tag {
 func (a *SetupService) newTagList(config *client.EventStreamConfig) []*cloudformation.Tag {
 	tags := make([]*cloudformation.Tag, 2)
 	keys := []string{"Version", "LastUpdatedTime"}
+	//Do not put comma in tag values due to aws internal bug.
 	values := []string{config.Version, time.Now().Format(time.RFC3339)}
 	for i := range tags {
 		tags[i] = a.newTag(keys[i], values[i])
