663a6f40b14f65f5b40969d0d918e01d6fdaff94
Zechen Jiang <jiangz@vmware.com>
2019-10-22 16:21:32 -0700

    PLA-11482: Remove team id in wrapper script

--- scripts/cloud_add_wrapper_aws.sh
+++ scripts/cloud_add_wrapper_aws.sh
@@ -6,7 +6,7 @@
 IFS=","
 team_id="YOUR_TEAM_ID"
 role_name="securestate_role"
-accounts=$(vss cloud list --json --team-id $team_id | jq -r .[].name)
+accounts=$(vss cloud list --json | jq -r .[].name)
 while read account_name account_id environment profile; do
 	echo "creating role for account: $account_id"
 	is_exist=false
@@ -19,11 +19,11 @@ while read account_name account_id environment profile; do
 	done <<< "$accounts"
 	if [ $is_exist = true ]; then
 		echo "Cloud account with name $account_name already exist, skip for this account"
-		cloud_id=$(vss cloud list --json --team-id $team_id | jq  -r '.[] | select(.name=="'$account_name'")|._id')
-		vss event setup --team-id $team_id --cloud-id $cloud_id --aws-profile $profile --ignore-missing-trails
+		cloud_id=$(vss cloud list --json | jq  -r '.[] | select(.name=="'$account_name'")|._id')
+		vss event setup --cloud-id $cloud_id --aws-profile $profile --ignore-missing-trails
 		continue
 	fi
-	cloud_id=$(vss cloud add --team-id $team_id --name $account_name --role $role_name --aws-profile $profile --environment $environment --json | jq -r ._id)
-	vss event setup --team-id $team_id --cloud-id $cloud_id --aws-profile $profile --ignore-missing-trails
+	cloud_id=$(vss cloud add --name $account_name --role $role_name --aws-profile $profile --environment $environment --json | jq -r ._id)
+	vss event setup --cloud-id $cloud_id --aws-profile $profile --ignore-missing-trails
 done
 unset IFS
--- scripts/cloud_add_wrapper_azure.sh
+++ scripts/cloud_add_wrapper_azure.sh
@@ -5,7 +5,7 @@
 IFS=", "
 team_id="YOUR_TEAM_ID"
 directory_id="YOUR_DIRECTORY_ID"
-accounts=$(vss cloud list --json --team-id $team_id | jq -r .[].name)
+accounts=$(vss cloud list --json | jq -r .[].name)
 while read account_name application_id key_value subscription_id environment; do
 	echo "Adding account: $subscription_id"
 	is_exist=false
@@ -18,11 +18,11 @@ while read account_name application_id key_value subscription_id environment; do
 	done <<< "$accounts"
 	if [ $is_exist = true ]; then
 		echo "Cloud account with name $account_name already exist, skip for this account"
-		cloud_id=$(vss cloud list --json --team-id $team_id | jq  -r '.[] | select(.name=="'$account_name'")|._id')
-		vss event setup --team-id $team_id --cloud-id $cloud_id
+		cloud_id=$(vss cloud list --json | jq  -r '.[] | select(.name=="'$account_name'")|._id')
+		vss event setup --cloud-id $cloud_id
 		continue
 	fi
-	cloud_id=$(vss cloud add --team-id $team_id --name $account_name --application-id $application_id --key-value $key_value --subscription-id $subscription_id --directory-id $directory_id  --environment $environment --provider Azure --json | jq -r ._id)
-	vss event setup --team-id $team_id --cloud-id $cloud_id
+	cloud_id=$(vss cloud add --name $account_name --application-id $application_id --key-value $key_value --subscription-id $subscription_id --directory-id $directory_id  --environment $environment --provider Azure --json | jq -r ._id)
+	vss event setup --cloud-id $cloud_id
 done
 unset IFS
